58ec53c18ba8b7987cc92ab2af90a0effd4b98da
Rmartinez75 <rmartinez75@gmail.com>
2020-05-24 09:55:29 -0400

    This commit will add an array of Pokemon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-alert("Hello World");
-var favoriteFood = "Pizza!";
-document.write(favoriteFood);
+var pokemonList = [
+  {
+    name: "Pikachu",
+    height: 1,
+    types: ["Flying", "Steel", "Electric"],
+  },
+  {
+    name: "Mewtwo",
+    height: 2,
+    types: ["Ghost", "Fighting", "Psychic"],
+  },
+  {
+    name: "Pidgeot",
+    height: 1,
+    types: ["Rock", "Ice", "Grass"],
+  },
+];
