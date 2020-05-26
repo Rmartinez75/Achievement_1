ca9f16e0aeca7e6ddc7bd1633d18daebe0521593
Rmartinez75 <rmartinez75@gmail.com>
2020-05-26 15:39:16 -0400

    This commit puts pokemonList into an IIFE function which allowed me to shorten some code. Also changed some of the style.

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,49 @@
-//List of pokemon with some of their attributes
-var pokemonList = [
-  { name: 'Pikachu', height: 3, types: ['Flying', 'Steel', 'Electric'] },
-  { name: 'Mewtwo', height: 7, types: ['Ghost', 'Fighting', 'Psychic'] },
-  { name: 'Pidgeot', height: 3, types: ['Rock', 'Ice', 'Grass'] },
-  { name: 'Charizard', height: 6, types: ['Rock', 'Ground', 'Fire'] },
-];
+//List of pokemon with some of their attributes using an IIFE
+var pokemonRepository = (function () {
+  var pokemonList = [
+    { name: 'Pikachu', height: 3, types: ['Flying', 'Steel', 'Electric'] },
+    { name: 'Pidgeot', height: 3, types: ['Rock', 'Ice', 'Grass'] },
+    { name: 'Mewtwo', height: 7, types: ['Ghost', 'Fighting', 'Psychic'] },
+    { name: 'Charizard', height: 6, types: ['Rock', 'Ground', 'Fire'] },
+  ];
 
-//Loops over the pokemonList with forEach method then writes onto DOM along with height
-pokemonList.forEach(function (pokemon) {
-  if (pokemon.height > 3) {
-    document.write(
-      '<p>' +
-        '** ' +
-        pokemon.name +
-        ' **' +
-        '<br>' +
-        'Height: ' +
-        pokemon.height +
-        " -- Now he's tall!" +
-        '</p>'
-    );
-  } else {
-    document.write(
-      '<p>' +
-        '** ' +
-        pokemon.name +
-        ' **' +
-        '<br>' +
-        'Height: ' +
-        pokemon.height +
-        '</p>'
-    );
+  function add(item) {
+    if (typeof item === 'object') {
+      pokemonList.push(item);
+    } else {
+      document.write('This is not a Pokemon!');
+    }
   }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
+
+//Loops over the pokemonRepository with forEach method using getAll() function thats in the IIFE. Then checks if the data thats being input is an object, then writes onto DOM along with height
+pokemonRepository.getAll().forEach(function (pokemon) {
+  var tall;
+  if (pokemon.height > 3) {
+    tall = "--Wow that's tall!--";
+  } else {
+    tall = '--Too short!--';
+  }
+
+  document.write(
+    '<div class="container">' +
+      '** ' +
+      pokemon.name +
+      ' **' +
+      '<br>' +
+      'Height: ' +
+      pokemon.height +
+      '<br>' +
+      tall +
+      '</div>'
+  );
 });
