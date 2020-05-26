f0cac6f65505866c9f8325eeb3ca74e8fc676ddc
Rmartinez75 <rmartinez75@gmail.com>
2020-05-26 07:14:26 -0400

    This commit changed the for loop to a forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,35 @@
 //List of pokemon with some of their attributes
 var pokemonList = [
-  { name: "Pikachu", height: 3, types: ["Flying", "Steel", "Electric"] },
-  { name: "Mewtwo", height: 7, types: ["Ghost", "Fighting", "Psychic"] },
-  { name: "Pidgeot", height: 3, types: ["Rock", "Ice", "Grass"] },
-  { name: "Charizard", height: 6, types: ["Rock", "Ground", "Fire"] },
+  { name: 'Pikachu', height: 3, types: ['Flying', 'Steel', 'Electric'] },
+  { name: 'Mewtwo', height: 7, types: ['Ghost', 'Fighting', 'Psychic'] },
+  { name: 'Pidgeot', height: 3, types: ['Rock', 'Ice', 'Grass'] },
+  { name: 'Charizard', height: 6, types: ['Rock', 'Ground', 'Fire'] },
 ];
 
-//Loops over the pokemonList then writes onto DOM along with height
-for (i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 3) {
+//Loops over the pokemonList with forEach method then writes onto DOM along with height
+pokemonList.forEach(function (pokemon) {
+  if (pokemon.height > 3) {
     document.write(
-      "<p>" +
-        "** " +
-        pokemonList[i].name +
-        " **" +
-        "<br>" +
-        "Height: " +
-        pokemonList[i].height +
+      '<p>' +
+        '** ' +
+        pokemon.name +
+        ' **' +
+        '<br>' +
+        'Height: ' +
+        pokemon.height +
         " -- Now he's tall!" +
-        "</p>"
+        '</p>'
     );
   } else {
     document.write(
-      "<p>" +
-        "** " +
-        pokemonList[i].name +
-        " **" +
-        "<br>" +
-        "Height: " +
-        pokemonList[i].height +
-        "</p>"
+      '<p>' +
+        '** ' +
+        pokemon.name +
+        ' **' +
+        '<br>' +
+        'Height: ' +
+        pokemon.height +
+        '</p>'
     );
   }
-}
+});
